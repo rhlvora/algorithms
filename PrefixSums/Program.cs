209e14cad2105d1894c94f001728d483f021ee3a
Rahul Vora <rhlvora@gmail.com>
2021-03-07 00:01:42 -0700

    playing around with references

--- PrefixSums/Program.cs
+++ PrefixSums/Program.cs
@@ -26,21 +26,53 @@ namespace PrefixSums
 			int k = 3;
 			int m = 6;
 
+			int max = 0;
+			int maxi = 0;
+			int maxj = 0;
+
 			//move to the right by i, then move to the left by m-i
-			for(int i = 0; i < m; i++)
+			for(int i = 0; i <= m; i++)
 			{
 				//number of moves the other direction
 				int j = m-i;
+
+				//move right then left
+				Console.WriteLine($"Move right {i} move left {j}");
+				int temp = CalcNumberMushrooms(A, k, i, j, 1);
+				if(temp > max)
+				{
+					max = temp;
+					maxi = i;
+					maxj = j;
+				}
+
+				//move left then right
+				Console.WriteLine($"Move left {i} move right {j}");
+				temp = CalcNumberMushrooms(A, k, i, j, -1);
+				if(temp > max)
+				{
+					max = temp;
+					maxi = i;
+					maxj = j;
+				}
 			}
 
-			Console.WriteLine(CalcNumberMushrooms(A, 3, 2, 2));
+			//Console.WriteLine(CalcNumberMushrooms(A, 3, 2, 2));
+
+			Console.WriteLine($"Maximum number of mushrooms is {max} in {6} moves");
+			Console.WriteLine($"Move right {maxi} move left {maxj}");
 		}
 
 		//move i to the right
 		//move j to the left
 		//if I go beyond the boundary of the array, add 0s
-		static int CalcNumberMushrooms(int[] A, int k, int i, int j)
+		static int CalcNumberMushrooms(int[] A, int k, int i, int j, int dir)
 		{
+			foreach(var q in A)
+			{
+				Console.Write($"{q} ");
+			}
+
 			//assume you pick up the mushrooms at the starting spot
 			int Acc = A[k];
 			A[k] = 0;
@@ -50,13 +82,14 @@ namespace PrefixSums
 			//move to the right
 			for(int x = 0; x < i; x++)
 			{
-				k++;
+				k = k + dir;
 				//if we haven't gone off the edge to the right
-				if(k < A.Length)
+				if(k >= 0 && k < A.Length)
 				{
-					Console.WriteLine("right " + k);
-
 					Acc = Acc + A[k];
+
+					Console.WriteLine($"Position: {k} Acc: {Acc}");
+
 					//don't want to double count, set it to 0
 					A[k] = 0;
 				}
@@ -65,18 +98,21 @@ namespace PrefixSums
 			//move to the left
 			for(int x = j; x > 0; x--)
 			{
-				k--;
+				k = k - dir;
 				//if we haven't gone off the edge to the left
-				if(k > 0 && k < A.Length)
+				if(k >= 0 && k < A.Length)
 				{
-					Console.WriteLine("left " + k);
 
 					Acc = Acc + A[k];
+
+					Console.WriteLine($"Position: {k} Acc: {Acc}");
 
 					A[k] = 0;
 				}
 
 			}
+
+			Console.WriteLine($"Collected {Acc} mushrooms");
 
 			return Acc;
 		}
---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[2] {10,10};
+
+
+			Console.WriteLine("array is passed as usual");
+			Console.WriteLine("we can change the values of the array");
+			Console.WriteLine("but we cannot change the actual array reference");
+
+			Console.WriteLine(Arr[0]);
+
+			DoSomething(Arr);
+
+			Console.WriteLine(Arr[0]);
+
+			Console.WriteLine("array ref is passed this time");
+			Console.WriteLine("this time we can change the reference itself");
+
+			Arr = new int[2] {10, 10};
+			Console.WriteLine(Arr[0]);
+
+			DoSomething2(ref Arr);
+
+			Console.WriteLine(Arr[0]);
+
+			
+        }
+
+
+		//testing reference type array
+		static void DoSomething(int[] arr)
+		{
+			//this sets arr[0] to 0
+			//because the reference is passed by value
+			//to actually change the reference itself, you have to use the ref tag
+			arr[0] = 0;
+
+			//this won't work
+			arr = new int[2] {3,3};
+		}
+
+		static void DoSomething2(ref int[] arr)
+		{
+			//this time use the ref
+
+			arr[0] = 0;
+
+			//this will work
+			arr = new int[2] {3,3};
+		}
+
+
+    }
+}
